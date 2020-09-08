cec413e78ed93bf85a5aa8023f95c8f34790b23c
Buzz-1 <buzzone100894@gmail.com>
2020-09-08 13:32:06 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+// Initialize Firebase
+
+
+///Edit Under This only
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAQdZ2efek_iBFz4AmjXfg_6viei49Y4tI",
+  authDomain: "hacking08-a2110.firebaseapp.com",
+  databaseURL: "https://hacking08-a2110.firebaseio.com",
+  projectId: "hacking08-a2110",
+  storageBucket: "hacking08-a2110.appspot.com",
+  messagingSenderId: "673145086217",
+  appId: "1:673145086217:web:f10bf48737e4dd99a9f3d8",
+  measurementId: "G-QGEWVDH9VM"
+};
+     
+ 
+////Do Not Edit Below This
+
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
